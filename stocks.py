f438c45c3626550ac6eb92f3f172fc92dc7bf57e
nealt <nealtrevor@gmail.com>
2022-06-04 12:11:33 -0600

    stock class cleanup

--- stocks.py
+++ stocks.py
@@ -9,33 +9,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 plt.style.use('seaborn')
-#import seaborn as sns
 import yfinance as yf
 import flask
 from flask import Flask, render_template # for web app
 from flask import Response
-
 import io
-#import random
 import plotly
 import plotly.express as px
 import json # for graph plotting in website
 
-
-
-
-
+#populate a few stocks for comparison.  can be cleared in the browser with the Clear button
 stock_symbol_list = ["SPY", "MSFT", "TSLA","GOOGL"]
 
+#set dates for different line charts
 today = datetime.today().date()
 one_year_date = (datetime.today().date()-timedelta(days=365))
 one_wk_date = (datetime.today().date()-timedelta(days=7))
 five_year_date = (datetime.today().date()-timedelta(days=1825))
 
-class Test:
-    def hello(name):
-        print("hello "+name)
 
+#stock class that users the ticker from the flask form to pull data from the yahoo finance api, populate a dataframe, then plot the data in a browser dashboard using plotly.
 class Stock:
     def stock_append(stock_ticker):
         stock_symbol_list.append(stock_ticker)
@@ -77,8 +70,6 @@ class Stock:
         for sym in stock_symbol_list:
         
           ticker_df = yf.Ticker(sym)
-          
-          
           #1wk
           ticker_close_df_wk = ticker_df.history(start=one_wk_date, end=today)
           ticker_close_df_wk['Symbol'] = sym
@@ -86,7 +77,6 @@ class Stock:
           
         
         #append to dfs
-        
         #wk
         appended_data_wk = pd.concat(appended_data_wk)
         #appended_data.to_csv('loop_bigDF.csv')
@@ -95,7 +85,6 @@ class Stock:
            
         # plot looped data
         #plotly
-    
         #wk
         all_stock_prices_wk = px.line(data_frame=(stock_df_wk),
                                     x='Date', y='Close', color='Symbol',
@@ -108,17 +97,12 @@ class Stock:
     def stock_data_5yr():
         appended_data_five_yr = []
         for sym in stock_symbol_list:
-        
           ticker_df = yf.Ticker(sym)
-          
-          
           #5yr
           ticker_close_df_5yr = ticker_df.history(start=five_year_date, end=today)
           ticker_close_df_5yr['Symbol'] = sym
           appended_data_five_yr.append(ticker_close_df_5yr)
-        
         #append to dfs
-        
         #5yr
         appended_data_five_yr = pd.concat(appended_data_five_yr)
         #appended_data.to_csv('loop_bigDF.csv')
@@ -127,24 +111,20 @@ class Stock:
         
         # plot looped data
         #plotly
-        
         #5yr
         all_stock_prices_5yr = px.line(data_frame=(stock_df_5yr),
                                     x='Date', y='Close', color='Symbol',
                                     title='Stock Price History (5 Years)'
                                     )
         graph_5yr = json.dumps(all_stock_prices_5yr, cls=plotly.utils.PlotlyJSONEncoder)
-       
         return graph_5yr;
     
         
     def stock_analysis(stock_ticker):
         stock_symbol_list.append(stock_ticker)
-        ##############
-        # MOVE TO STOCKS.py stocks class
-        ######
+        ################################################
         #LOOP through list of stocks and append to DF
-        ######
+        ################################################
     
         appended_data_yr = []
         appended_data_wk = []
@@ -207,6 +187,6 @@ class Stock:
         graph_5yr = json.dumps(all_stock_prices_5yr, cls=plotly.utils.PlotlyJSONEncoder)
        
         return [graph_year, graph_week,graph_5yr];
-        #return render_template('stockhistory.html',graph_year=graph_year,graph_week=graph_week,graph_5yr=graph_5yr)#,table=parsed_and_scored_news.to_html(classes='data'))
+        
         
    
